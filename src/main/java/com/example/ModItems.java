c84cb9a0c9ba520a6a0f94a43d580267708cfa5b
Benjamin Nater <benjamin@nater.net>
2024-12-29 23:57:34 +0100

    init

---
+++ src/main/java/com/example/ModItems.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SUSPICIOUS_SUBSTANCE = register(
+            new Item.Settings().equippable(EquipmentSlot.CHEST),
+            "suspicious_substance"
+    );
+
+    private static Item register(Item.Settings itemSettings, String name) {
+        Identifier id = Identifier.of(TemplateMod.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = itemSettings.registryKey(key);
+
+        return Registry.register(Registries.ITEM, key, new Item(settings));
+    }
+
+
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+// And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+    }
+}
